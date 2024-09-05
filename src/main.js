2079ff60ef86d35c3b34bb02a28a4b0913989a8c
yelyzaveta-sokolova <yelyzavetasokolova71@gmail.com>
2024-09-05 23:57:28 +0200

    add partials

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import './js/header';
+import './js/hero';
+import './js/about-me';
+import './js/benefits';
+import './js/projects';
+import './js/FAQ';
+import './js/covers';
+import './js/reviews';
+import './js/footer';
+import './js/modale-window';
+import './js/mobile-menu';
+
+
+
