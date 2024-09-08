9a1f48e0af22c8974d17578145e636c079288642
Oleksandr-Kalnaus <oleksandrkalnaus@gmail.com>
2024-09-08 22:40:14 +0100

    Edit modal window js
    Reworked the code and added closing the modal window with the esc key

--- src/js/modal-window.js
+++ src/js/modal-window.js
@@ -7,17 +7,37 @@ const FORM = document.querySelector('.js-footer-form');
 SEND_BTN.onclick = function(event) {
     event.preventDefault();
 
-        MODAL_WINDOW.style.display = 'block';
-        MODAL_OVERLAY.style.display = 'block';
+    MODAL_WINDOW.style.display = 'block';
+    MODAL_OVERLAY.style.display = 'block';
 
-        setTimeout(() => {
-            MODAL_WINDOW.classList.add('show');
-            MODAL_OVERLAY.classList.add('show');
-        }, 10);
-    
-}
+    setTimeout(() => {
+        MODAL_WINDOW.classList.add('show');
+        MODAL_OVERLAY.classList.add('show');
+    }, 10);
+};
 
 CLOSE_BTN.onclick = function() {
+    closeModal();
+};
+
+window.onclick = function(event) {
+    if (event.target == MODAL_OVERLAY) {
+        closeModal();
+        FORM.reset();
+    }
+};
+
+window.addEventListener('keydown', (event) => {
+    if (event.key === 'Escape') {
+        closeModal();
+    }
+});
+
+CLOSE_BTN.addEventListener('click', () => {
+    FORM.reset();
+});
+
+function closeModal() {
     MODAL_WINDOW.classList.remove('show');
     MODAL_OVERLAY.classList.remove('show');
 
@@ -26,27 +46,3 @@ CLOSE_BTN.onclick = function() {
         MODAL_OVERLAY.style.display = 'none';
     }, 1000);
 }
-
-window.onclick = function(event) {
-    if (event.target == MODAL_OVERLAY) {
-        MODAL_WINDOW.classList.remove('show');
-        MODAL_OVERLAY.classList.remove('show');
-
-        setTimeout(() => {
-            MODAL_WINDOW.style.display = 'none';
-            MODAL_OVERLAY.style.display = 'none';
-        }, 1000);
-
-        FORM.reset();
-    }
-}
-
-CLOSE_BTN.addEventListener('click', () => {
-    FORM.reset();
-});
-
-window.addEventListener('click', (event) => {
-    if (event.target == MODAL_OVERLAY) {
-        FORM.reset();
-    }
-});
