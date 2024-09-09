758567c282606ce8b9304074df247b3cab6d6fae
Anna D <altavanamail@gmail.com>
2024-09-09 07:56:28 +0300

    add section covers

---
+++ src/js/covers.js
@@ -0,0 +1,20 @@
+const SECTION = document.querySelector('.js-observered');
+
+const OBSERVE_OPTIONS = {
+  root: null,
+  rootMargin: '0px 0px 0px 0px',
+  threshold: 0.5,
+};
+const OBSERVER_CALLBACK = entries => {
+  if (entries[0].isIntersecting) {
+    const TAGS = document.getElementsByClassName('marquee-line');
+    Array.from(TAGS).forEach(
+      tag => (tag.style = 'animation-name: marqueeLine')
+    );
+  } else {
+    const TAGS = document.getElementsByClassName('marquee-line');
+    Array.from(TAGS).forEach(tag => (tag.style = 'animation-name: none'));
+  }
+};
+const OBSERVER = new IntersectionObserver(OBSERVER_CALLBACK, OBSERVE_OPTIONS);
+OBSERVER.observe(SECTION);
