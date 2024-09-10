673dc40215e2fb41f5df2b428e2dd5ee6ed4d508
Andrii Sukhovetskyi <andreysukhovetskyi@gmail.com>
2024-09-10 19:02:35 +0200

    fix v0.1

--- src/js/FAQ.js
+++ src/js/FAQ.js
@@ -1,8 +1,12 @@
 import Accordion from 'accordion-js';
 
   document.addEventListener("DOMContentLoaded", function () {
-    new Accordion('.accordion-container', {
+    new Accordion('.faq-accordion-container', {
       duration: 600,
       showMultiple: true,
+      triggerClass: 'faq-ac-trigger',
+      elementClass: 'faq-ac',
+      panelClass: 'faq-ac-panel',
+      activeClass: 'faq-is-active',
     });
   });
