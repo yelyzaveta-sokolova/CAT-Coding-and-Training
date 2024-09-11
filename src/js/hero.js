8c48af7014a20c0c5ae709c93731e3f26feab817
yelyzaveta-sokolova <yelyzavetasokolova71@gmail.com>
2024-09-11 22:19:11 +0200

    remove js-hero

--- src/js/hero.js
+++ src/js/hero.js
@@ -1,16 +1,16 @@
-const SCROLL_BTN_HERO = document.querySelector('.btn-scroll-top');
+// const SCROLL_BTN_HERO = document.querySelector('.btn-scroll-top');
 
-window.addEventListener('scroll', () => {
-  if (window.scrollY > 300) {
-    SCROLL_BTN_HERO.classList.remove('is-btnshow-hide');
-  } else {
-    SCROLL_BTN_HERO.classList.add('is-btnshow-hide');
-  }
-});
+// window.addEventListener('scroll', () => {
+//   if (window.scrollY > 300) {
+//     SCROLL_BTN_HERO.classList.remove('is-btnshow-hide');
+//   } else {
+//     SCROLL_BTN_HERO.classList.add('is-btnshow-hide');
+//   }
+// });
 
-SCROLL_BTN_HERO.onclick = () => {
-  window.scrollTo({
-    top: 0,
-    behavior: 'smooth',
-  });
-};
+// SCROLL_BTN_HERO.onclick = () => {
+//   window.scrollTo({
+//     top: 0,
+//     behavior: 'smooth',
+//   });
+// };
