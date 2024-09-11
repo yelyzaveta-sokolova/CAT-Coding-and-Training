73ee1df3b6f5d75e460ceeef5b732981a998d92b
yelyzaveta-sokolova <yelyzavetasokolova71@gmail.com>
2024-09-11 22:50:22 +0200

    correct import swiper

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -1,5 +1,5 @@
-import Swiper from 'swiper/bundle';
-import "swiper/css/bundle";
+import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs';
+import "https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css";
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
