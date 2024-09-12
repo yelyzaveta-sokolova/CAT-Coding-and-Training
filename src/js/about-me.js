e92d93407c16377134790723f0823a9d3c5bfabe
Andrii Sukhovetskyi <andreysukhovetskyi@gmail.com>
2024-09-12 21:22:05 +0200

    about-me fix by Andrii

--- src/js/about-me.js
+++ src/js/about-me.js
@@ -1,35 +1,35 @@
 import Accordion from 'accordion-js';
 
-const ACCORDION = new Accordion('.about-me-accord-list', {
-  duration: 350,
+ document.addEventListener("DOMContentLoaded", function () {
+   new Accordion('.about-me-accord-list',{
+  duration: 600,
   elementClass: 'about-me-accord-item',
   triggerClass: 'about-me-accord-trigger',
   panelClass: 'about-me-accord-panel',
   showMultiple: true,
-  onOpen: onOpenHandle,
-});
+  openOnInit: [0],
+   });
+  });
+// ACCORDION.open(0);
 
-ACCORDION.open(0);
+// setTimeout(() => {
+//   ACCORDION.attachEvents();
+// }, 1000);
 
-setTimeout(() => {
-  ACCORDION.attachEvents();
-}, 1000);
+// function onOpenHandle(currElement) {
+//   const ACTIVEACRECT = currElement.getBoundingClientRect();
 
-function onOpenHandle(currElement) {
-  const ACTIVEACRECT = currElement.getBoundingClientRect();
+//   const viewHeight = Math.max(
+//     document.documentElement.clientHeight,
+//     window.innerHeight
+//   );
 
-  const viewHeight = Math.max(
-    document.documentElement.clientHeight,
-    window.innerHeight
-  );
-
-  if (ACTIVEACRECT.bottom > viewHeight) {
-    window.scrollTo({
-      top:
-        ACTIVEACRECT.top +
-        window.scrollY -
-        (viewHeight - ACTIVEACRECT.height) / 2,
-      behavior: 'smooth',
-    });
-  }
-}
+//   if (ACTIVEACRECT.bottom > viewHeight) {
+//     window.scrollTo({
+//       top:
+//         ACTIVEACRECT.top +
+//         window.scrollY -
+//         (viewHeight - ACTIVEACRECT.height) / 2,
+//       behavior: 'smooth',
+//     });
+//   }
