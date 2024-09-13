2c2bb990ae3e9c4a2d3e3ed9c1eab92d7a1380a6
yelyzaveta-sokolova <yelyzavetasokolova71@gmail.com>
2024-09-13 02:44:09 +0200

    clear files and add header

--- src/main.js
+++ src/main.js
@@ -2,14 +2,12 @@ import './js/header';
 import './js/hero';
 import './js/about-me';
 import './js/about-me-skills';
-import './js/benefits';
 import './js/projects';
 import './js/FAQ';
 import './js/covers';
 import './js/reviews';
 import './js/footer';
 import './js/modal-window';
-import './js/mobile-menu';
 
 
 
